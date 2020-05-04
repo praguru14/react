d1cdbd16deac73ae9ac59fc31701d2bb9ab654ba
praguru14 <48213609+praguru14@users.noreply.github.com>
2020-05-04 12:47:15 +0530

    Updates

---
+++ precache-manifest.28eca52003bacb98c82ee63623b53266.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7da9a40ebbb21dddfc6135687098ef55",
+    "url": "/react/index.html"
+  },
+  {
+    "revision": "f554b4fe4a6b199ac6ae",
+    "url": "/react/static/css/2.e944007b.chunk.css"
+  },
+  {
+    "revision": "bce4d7b4f3062c1999c0",
+    "url": "/react/static/css/main.82aaaa86.chunk.css"
+  },
+  {
+    "revision": "f554b4fe4a6b199ac6ae",
+    "url": "/react/static/js/2.5ef710ca.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react/static/js/2.5ef710ca.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bce4d7b4f3062c1999c0",
+    "url": "/react/static/js/main.45760448.chunk.js"
+  },
+  {
+    "revision": "be18c2fb57f3c591ea50",
+    "url": "/react/static/js/runtime-main.95d8c859.js"
+  }
+]);
